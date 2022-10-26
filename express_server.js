529203bc3b21054620b9fbe3f3fafb7293fc883a
Gozi1 <snwabiani@gmail.com>
2022-10-26 04:41:03 +0000

    added set fetch exmaple to express server

--- express_server.js
+++ express_server.js
@@ -22,3 +22,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });

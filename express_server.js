094fd3262144902eed6ee1c67dfeeefe3eb8d1ea
Gozi1 <snwabiani@gmail.com>
2022-10-26 18:30:02 +0000

    can add urls now

--- express_server.js
+++ express_server.js
@@ -4,10 +4,18 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  //length of the string 
+  const len = 6;
+  return Math.random().toString(36).substring(2,len+2);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,10 +42,19 @@ app.get("/set", (req, res) => {
   res.render("urls_index",templateVars);
  });
 
+ app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
  app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id] };
   res.render("urls_show",templateVars);
  });
+
+ app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
  app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

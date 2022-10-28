a91e9aeca450141119a547ff264987e857f21558
Gozi1 <snwabiani@gmail.com>
2022-10-28 18:34:07 +0000

    Add POST route for /login to expressserver.js Redirect browser back to /urls page after successful login Add a POST route for /logout which clears the cookie and redirects user to /urls page

--- express_server.js
+++ express_server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const  cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,62 +19,85 @@ const randomString = function generateRandomString() {
   const len = 6;
   return Math.random().toString(36).substring(2,len+2);
 }
+//I dont think I need these but I wasnt told to remove them so yh ...
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+//  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+//  });
 
  app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+     };
   res.render("urls_index",templateVars);
  });
 
  app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {  
+    username: req.cookies["username"]
+     };
+  res.render("urls_new",templateVars);
 });
 
  app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL:urlDatabase[req.params.id],
+    username: req.cookies["username"] };
   res.render("urls_show",templateVars);
  });
+
+ app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
 
  app.post("/urls", (req, res) => {
   // appends a random string to the posted url
   const shortUrl = randomString();
   urlDatabase[shortUrl] = req.body.longURL;
-  const templateVars = { id: shortUrl, longURL:urlDatabase[shortUrl] };
+  const templateVars = { 
+    id: shortUrl, 
+    longURL:urlDatabase[shortUrl],
+    username: req.cookies["username"] };
   res.render("urls_show",templateVars); 
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  // appends a random string to the posted url
+  
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 app.post("/urls/:id/update", (req, res) => {
-  // appends a random string to the posted url
+  
   urlDatabase[req.params.id] = req.body.newURL;
   res.redirect(`/urls/${req.params.id}`);
 });
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+app.post("/login", (req, res) => {
+  
+  res.cookie('username',req.body.username);
+  console.log(req.body.username);
+  res.redirect(`/urls`);
+});
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect(`/urls`);
 });
 
  app.listen(PORT, () => {

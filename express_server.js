b5ab2e77d6653104516a302c888c397afe4d63aa
Gozi1 <snwabiani@gmail.com>
2022-11-03 03:12:35 +0000

    add post route for register and updated some endpoints

--- express_server.js
+++ express_server.js
@@ -9,54 +9,51 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const randomString = function generateRandomString() {
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+
+
+const randomID = ()=>{
   //length of the string 
   const len = 6;
   return Math.random().toString(36).substring(2,len+2);
 }
-//I dont think I need these but I wasnt told to remove them so yh ...
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
- 
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
 
  app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase, 
-    username: req.cookies["username"]
+    user: req.cookies['user_id']
      };
   res.render("urls_index",templateVars);
  });
 
  app.get("/urls/new", (req, res) => {
   const templateVars = {  
-    username: req.cookies["username"]
+    user: req.cookies['user_id']
      };
   res.render("urls_new",templateVars);
 });
 app.get("/register", (req, res) => {
   const templateVars = {  
-    username: req.cookies["username"]
+    user: req.cookies['user_id']
      };
   res.render("urls_register",templateVars);
 });
@@ -66,7 +63,7 @@ app.get("/register", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
     longURL:urlDatabase[req.params.id],
-    username: req.cookies["username"] };
+    user: req.cookies['user_id'] };
   res.render("urls_show",templateVars);
  });
 
@@ -77,12 +74,12 @@ app.get("/register", (req, res) => {
 
  app.post("/urls", (req, res) => {
   // appends a random string to the posted url
-  const shortUrl = randomString();
+  const shortUrl = randomID();
   urlDatabase[shortUrl] = req.body.longURL;
   const templateVars = { 
     id: shortUrl, 
     longURL:urlDatabase[shortUrl],
-    username: req.cookies["username"] };
+    user: req.cookies['user_id'] };
   res.render("urls_show",templateVars); 
 });
 
@@ -91,19 +88,36 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
+
 app.post("/urls/:id/update", (req, res) => {
   
   urlDatabase[req.params.id] = req.body.newURL;
   res.redirect(`/urls/${req.params.id}`);
 });
+
 app.post("/login", (req, res) => {
   
-  res.cookie('username',req.body.username);
-  console.log(req.body.username);
+  res.cookie('user_id',{email:req.body.user});
+
+  console.log(req.body.user);
   res.redirect(`/urls`);
 });
+
+app.post("/register", (req, res) => {
+  console.log("herer");
+  const randomUserID = randomID();
+  users[`user${randomUserID}ID`] = {
+    id: `user${randomUserID}ID`,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  console.log(users[`user${randomUserID}ID`]);
+  res.cookie('user_id',users[`user${randomUserID}ID`]);
+  res.redirect('/register');
+});
+
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect(`/urls`);
 });
 
